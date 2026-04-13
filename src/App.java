d4cac58a2fdb7cf4e4e80522fc7cf6cbe512a5a8
GnatGo <natngo858@gmail.com>
2026-04-13 17:58:28 +0000

    Started testing some and added rarity to cards

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //initial cards-----------------------------------
+        Card mew = new Card("Mew", "super rare");
+        Card bulbasaur = new Card("Bulbasaur", "rare");
+        Card altaria = new Card("Altaria", "rare");
+        Card starly = new Card("Starly", "common");
+        Card wingull = new Card("Wingull", "common");
+        Card psyduck = new Card("Psyduck", "common");
+
+        CardPack pack = new CardPack(15, 3);
+
     }
 }
--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,17 @@
 public class Card {
     private String name;
+    private String rarity;
 
-    public Card(String newName){
+    public Card(String newName, String newRarity){
         name = newName;
+        rarity = newRarity;
     }
 
     public String getName(){
         return name;
+    }
+
+    public String getRarity(){
+        return rarity;
     }
 }
