86d02d6ee6efab754029985a6079390bcd7ba1df
GnatGo <natngo858@gmail.com>
2026-04-16 16:20:31 +0000

    Can generate random cards with csv file

--- src/App.java
+++ src/App.java
@@ -1,26 +1,38 @@
+import java.io.File;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        //initial cards-----------------------------------
-        Card mew = new Card("Mew", "super rare");
-        Card bulbasaur = new Card("Bulbasaur", "rare");
-        Card altaria = new Card("Altaria", "rare");
-        Card starly = new Card("Starly", "common");
-        Card wingull = new Card("Wingull", "common");
-        Card psyduck = new Card("Psyduck", "common");
+        //set up file and scanner stuff------------------------
+        File file = new File("src/Data/PokemonList.csv");
+        Scanner scanner = new Scanner(file);
+        Card[] pokemonCardArray = new Card[40];
+        int i = 0;
+        scanner.nextLine();
+        //put csv file into pokemonLines array--------------------
+        while (scanner.hasNext() && i < 40){
+            String line = scanner.nextLine();
+            //split up csv file and create card object to put in array--------
+            String[] data = line.split(",");
+            String name = data[1];
+            String rarity = data[2];
+            Card newCard = new Card(name, rarity);
+            pokemonCardArray[i] = newCard;
+            i++;
+        }
+        scanner.close();
 
-        CardPack pack = new CardPack(15, 3);
-
+        //user input--------------------------------
         Collection collection = new Collection();
-
-        System.out.println("Adding cards: ");
-        pack.addCard(altaria);
-        pack.addCard(bulbasaur);
-        pack.addCard(mew);
-
-        System.out.println("Opening pack: ");
-        Card[] opened = pack.openPack();
-        collection.addCards(opened);
+        CardPack pack = new CardPack(15, 17);
+        for (int j = 0; j < 17; j++){
+            int rand = (int)(Math.random() * 40);
+            pack.addCard(pokemonCardArray[rand]);
+        }
+        Card[] openCards = pack.openPack();
+        collection.addCards(openCards);
         collection.displayCollection();
+
+
 
 
 
