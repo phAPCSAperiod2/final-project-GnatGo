74282c4ac6bb166df1aafd4fe7f312e92b8f2e3b
GnatGo <natngo858@gmail.com>
2026-04-22 10:40:57 -0700

    Got the images scaled and to display correctly, need to work on finding urls that will actually display

--- src/Collection.java
+++ src/Collection.java
@@ -1,6 +1,11 @@
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -40,18 +45,36 @@ public class Collection {
 
     public void displayCollectionImage(){
         JFrame frame = new JFrame("Collection");
-        frame.setSize(800, 800);
+        frame.setSize(800, 1200);
         //method that sets jframe into 4 by 4 grid
         frame.setLayout(new GridLayout(4, 4));
 
         for (int i = 0; i < category.size() && i < 16; i++){
             try {
-                String url = category.get(i).getCard().getImage();
+                URL url = new URL(category.get(i).getCard().getImage());
+                BufferedImage originalImage = ImageIO.read(url);
 
-                ImageIcon icon = new ImageIcon(new URL(url));
-                JLabel label = new JLabel(icon);
+                //scale image
+                int newWidth = 200;
+                int newHeight = 300;
+                //SCALE_SMOOTH prioritizes making the image look smooth
+                Image scaledImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
 
-                frame.add(label);
+                //convert back to bufferedimage to be displayed
+                BufferedImage originalScaled = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+                Graphics2D g2d = originalScaled.createGraphics();
+                g2d.drawImage(scaledImage, 0, 0, null);
+                g2d.dispose();
+
+                //display-----------------------
+                frame.add(new JLabel(new ImageIcon(originalScaled)));
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //.pack() scales frame to fit window size
+                frame.pack();
+                frame.setVisible(true);
+
+
+
             }
             catch (Exception e){
                 frame.add(new JLabel("Error"));
